8cda59c1055f632dea944595be172274bba69183
Neetu Kumari <52626507+neetusuthar90@users.noreply.github.com>
2023-03-16 15:28:42 +0530

    Added solution - LeetHub

---
+++ Postfix to Prefix Conversion - GFG/postfix-to-prefix-conversion.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    string postToPre(string post_exp) {
+        // Write your code here
+        int n = post_exp.length();
+        stack<string> st;
+        
+        for(int i = 0; i < n; i++){
+            char x = post_exp[i];
+            if(x >= 'a' && x <= 'z' || x >= 'A' && x <= 'Z'){
+                string op(1,x);
+                st.push(op);
+            }
+            else{
+                string s1 = st.top();
+                st.pop();
+                string s2 = st.top();
+                st.pop();
+                st.push(x+s2+s1);
+            }
+        }
+        
+        return st.top();
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+        string postfix;
+        cin >> postfix;
+
+        // char marker; cin >> marker;
+
+        Solution obj;
+        cout << obj.postToPre(postfix) << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+
+// } Driver Code Ends
