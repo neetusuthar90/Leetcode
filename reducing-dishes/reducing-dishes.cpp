ce82769b2250d4544cca31da067e38b581b726e8
Neetu Kumari <52626507+neetusuthar90@users.noreply.github.com>
2023-03-29 18:26:59 +0530

    Time: 56 ms (24.37%), Space: 20.8 MB (49.34%) - LeetHub

--- reducing-dishes/reducing-dishes.cpp
+++ reducing-dishes/reducing-dishes.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int maxSatisfaction(vector<int>& sat) {
-       int n =  sat.size();
-       sort(sat.begin(), sat.end());
-       int max_sum = 0;
-       int cum_sum = 0;
-       int cur_sum = 0;
-       for(int i = n-1; i >= 0; i--){
-           cur_sum = cur_sum + cum_sum + sat[i];
-           cum_sum = cum_sum + sat[i];
-           max_sum = max(max_sum, cur_sum);
-       }
-       return max_sum;
+    vector<vector<int>> dp;
+    int maxSatisfaction(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        dp.resize(n, vector<int> (n+1,-1));
+        return helper(0,1,arr);
+    }
+
+    int helper(int i, int t, vector<int> &arr){
+        if(i >= arr.size()) return 0;
+        if(dp[i][t] != -1) return dp[i][t];
+        int not_include = helper(i+1,t,arr);
+        int taken = arr[i]*t+ helper(i+1, t+1, arr);
+        return dp[i][t] = max(not_include, taken);
     }
 };
