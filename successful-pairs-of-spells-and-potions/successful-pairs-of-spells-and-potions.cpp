ffaec0bf810f8659b2b6186540c7fee8c1bd0a73
Neetu Kumari <52626507+neetusuthar90@users.noreply.github.com>
2023-04-02 13:50:17 +0530

    Time: 276 ms (24.72%), Space: 99.9 MB (51.66%) - LeetHub

--- successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
+++ successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -8,20 +8,8 @@ public:
         sort(potions.begin(), potions.end());
         for(int i = 0; i < n; i++){
             long c = (success+spells[i]-1)/spells[i];
-            int low = 0;
-            int high = m-1;
-            int idx = m;
-            while(low <= high){
-                int mid = low + (high-low)/2;
-                if(potions[mid] >= c){
-                    idx = mid;
-                    high = mid-1;
-                }
-                else{
-                    low = mid+1;
-                }
-            }
-            int count = m-idx;
+            auto idx = lower_bound(potions.begin(), potions.end(),c);
+            int count = potions.end()-idx;
             op.push_back(count);
         }
         return op;
