98dafdee3757cedd11e4a6ad9e42dd4b422018ab
Neetu Kumari <52626507+neetusuthar90@users.noreply.github.com>
2023-04-03 12:07:45 +0530

    Time: 75 ms (96.39%), Space: 44.9 MB (11.29%) - LeetHub

--- boats-to-save-people/boats-to-save-people.cpp
+++ boats-to-save-people/boats-to-save-people.cpp
@@ -2,16 +2,30 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         int n = people.size();
-        sort(people.begin(),people.end());
+        //Instead of using sorting we are using counting sort
+        vector<int> count(limit+1,0);
+        for(auto p:people){
+            count[p]++;
+        }
+        int idx = 0;
+        for(int val = 1; val <= limit; val++){
+            while(count[val] > 0){
+                people[idx++] = val;
+                count[val]--; 
+            }
+        }
         int low = 0;
         int high = n-1;
         int ans = 0;
         while(low <= high){
             if(people[low]+people[high] <= limit){
+                high--;
                 low++;
             }
+            else{
+                high--;
+            }
             ans++;
-            high--;
         }
         return ans;
     }
