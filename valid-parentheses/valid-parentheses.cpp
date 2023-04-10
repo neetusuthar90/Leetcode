e47e0a5e07d8d32dd6a632fe055c9cf35e48ed88
Neetu Kumari <52626507+neetusuthar90@users.noreply.github.com>
2023-04-10 20:36:29 +0530

    Time: 0 ms (100%), Space: 6.3 MB (56.25%) - LeetHub

--- valid-parentheses/valid-parentheses.cpp
+++ valid-parentheses/valid-parentheses.cpp
@@ -7,21 +7,19 @@ public:
         return false;
     }
     bool isValid(string s) {
-        if(s.length()==1){
-            return false;
-        }
         stack<char> st;
         for(int i = 0; i < s.length(); i++){
-            if(st.empty()||s[i]=='('||s[i]=='{'||s[i]=='['){
+            if(st.empty()&&(s[i]==']'||s[i]==')'||s[i]=='}')){
+                return false;
+            }
+            else if(s[i]=='('||s[i]=='['||s[i]=='{'){
                 st.push(s[i]);
             }
-            else if(!st.empty()){
-                if(isGoodPair(st.top(),s[i])){
-                    st.pop();
-                }
-                else{
-                    return false;
-                }
+            else if(isGoodPair(st.top(),s[i])){
+                st.pop();
+            }
+            else{
+                return false;
             }
         }
         if(st.empty()){
