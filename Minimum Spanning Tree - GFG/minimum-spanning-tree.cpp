f7288822dc84e4275d9c270043cfa6c830a2c83a
Neetu Kumari <52626507+neetusuthar90@users.noreply.github.com>
2023-04-15 11:45:23 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,42 +3,6 @@
 using namespace std;
 
 // } Driver Code Ends
-
-class DisjointSet
-{
-private:
-    vector<int> rank, parent;
-public:
-    DisjointSet(int n){
-        rank.resize(n+1,0);
-        parent.resize(n+1);
-        for(int i = 0; i <=n; i++){
-            parent[i] = i;
-        }
-    }
-
-    int findUPar(int node){
-        if(node == parent[node]) return node;
-        return parent[node] = findUPar(parent[node]);
-    }
-
-    void unionByRank(int u, int v){
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if(ulp_u == ulp_v) return;
-        if(rank[ulp_u] < rank[ulp_v]){
-            parent[ulp_u] = ulp_v;
-        }
-        else if(rank[ulp_v] < rank[ulp_u]){
-            parent[ulp_v] = ulp_u;
-        }
-        else{
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-};
-
 class Solution
 {
 	public:
@@ -46,61 +10,32 @@ class Solution
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        vector<pair<int, pair<int,int>>> edges;
-        //O(N+E)
-        for(int i = 0; i < V; i++){
-            for(auto it:adj[i]){
-                int adjNode = it[0];
-                int wt = it[1];
-                int node = i;
-                
-                edges.push_back({wt,{node,adjNode}});
-                //It is undirected graph but we don't need put other direction because we will use
-                // Disjoint set data structre and that will be take care of it
-            }
-        }
-        //O(E.log(E))
-        sort(edges.begin(), edges.end());
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        vector<int> vis(V,0);
+        pq.push({0,0});
+        int sum = 0;
         
-        DisjointSet ds(V);
-        int mstwt = 0;
-        //O((N+E).4*alpha)
-        for(auto it:edges){
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            int node = it.second;
             int wt = it.first;
-            int u = it.second.first;
-            int v = it.second.second;
+            if(vis[node]==1) continue;
+            //add it to mst
+            vis[node] = 1;
+            sum += wt;
             
-            if(ds.findUPar(u) != ds.findUPar(v)){
-                mstwt += wt;
-                ds.unionByRank(u,v);
+            for(auto nbd:adj[node]){
+                int adjNode = nbd[0];
+                int edW = nbd[1];
+                if(vis[adjNode]==0){
+                    pq.push({edW,adjNode});
+                }
             }
         }
-        return mstwt;
+        return sum;
     }
-    
-    //Time Complexity: O(E.log(V))
-    //Space Complexity: O(E+V)
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //{ Driver Code Starts.
 
