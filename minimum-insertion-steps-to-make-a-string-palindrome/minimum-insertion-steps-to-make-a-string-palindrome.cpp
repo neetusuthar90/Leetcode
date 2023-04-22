dcd2021fab327c6654dd51c8a6e9624adfa58340
Neetu Kumari <52626507+neetusuthar90@users.noreply.github.com>
2023-04-22 20:10:25 +0530

    Time: 108 ms (25.24%), Space: 28.6 MB (24.45%) - LeetHub

---
+++ minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        string s2 = s;
+        string s1 = s;
+        reverse(s1.begin(),s1.end());
+        int m = s.length();
+        vector<vector<int>> dp(m+1,vector<int>(m+1,-1));
+        
+        for(int i = 0; i <= m; i++) dp[i][0] = 0;
+        for(int j = 0; j <= m; j++) dp[0][j] = 0;
+
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= m; j++){
+                if(s1[i-1] == s2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                else{
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        return dp[m][m];
+    }
+    int minInsertions(string s) {
+        int n = s.length();
+        int lps_length = longestPalindromeSubseq(s);
+        return n-lps_length;
+    }
+};
