e8ee9f968e921009560c26f6df462215ac6b7ef3
Neetu Kumari <52626507+neetusuthar90@users.noreply.github.com>
2023-05-19 10:18:53 +0530

    Time: 31 ms (26.74%), Space: 13.6 MB (41.48%) - LeetHub

--- 00is-graph-bipartite/00is-graph-bipartite.cpp
+++ 00is-graph-bipartite/00is-graph-bipartite.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
+    //BFS TRAVERSAL
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> colorVec(n,-1);
-        queue<int> q;
+        vector<int> colour(n,0);
+        
 
-        for(int i = 0; i < n; i++){
-            if(colorVec[i] == -1){
-                colorVec[i] = 1;
-                q.push(i);
+        for(int node=0; node<n; node++){
+            if(colour[node]!=0) continue;
 
-                while(!q.empty()){
-                    int u = q.front();
-                    q.pop();
+            queue<int> q;
+            colour[node] = 1;
+            q.push(node);
 
-                    for(auto v:graph[u]){
-                        if(colorVec[v] == -1){
-                            colorVec[v] = 1 - colorVec[u];
-                            q.push(v);
-                        }
-                        else if(colorVec[v] == colorVec[u]){
-                            return false;
-                        }
+            while(!q.empty()){
+                int u = q.front();
+                q.pop();
+
+                for(auto v:graph[u]){
+                    if(colour[v]==0){
+                        //Not coloured yet
+                        colour[v] = -colour[u];
+                        q.push(v);
                     }
-                }        
+                    else if(colour[v] == colour[u]){
+                        // OR colour[v] != -colour[u]
+                        //Coloured is not as expected
+                        return false;
+                    }
+                }
             }
         }
         return true;
