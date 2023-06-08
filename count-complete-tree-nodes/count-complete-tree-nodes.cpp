51428236ee1c8d4f7c66e282058900ea5399a108
Neetu Kumari <52626507+neetusuthar90@users.noreply.github.com>
2023-06-08 11:11:43 +0530

    Time: undefined (undefined), Space: undefined (undefined) - LeetHub

--- count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -11,11 +11,34 @@
  */
 class Solution {
 public:
-    // Tree Traversal Technique
+   
+    int leftHeight(TreeNode* root){
+        int h = 0;
+        while(root){
+            h++;
+            root = root->left;
+        }
+        return h;
+    }
+    int rightHeight(TreeNode* root){
+        int h = 0;
+        while(root){
+            h++;
+            root = root->right;
+        }
+        return h;
+    }
     int countNodes(TreeNode* root) {
         if(root == NULL){
             return 0;
         }
+        int lh = leftHeight(root);
+        int rh = rightHeight(root);
+        
+        if(lh == rh){
+            return (1<<lh)-1;
+        }
+        
         int l = countNodes(root->left);
         int r = countNodes(root->right);
 
