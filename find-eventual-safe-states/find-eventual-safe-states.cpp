a7e88231003db55be61672a7d29eb2081789d8b5
Neetu Kumari <52626507+neetusuthar90@users.noreply.github.com>
2023-07-12 10:22:45 +0530

    Time: 226 ms (39.93%), Space: 58.9 MB (40.1%) - LeetHub

--- find-eventual-safe-states/find-eventual-safe-states.cpp
+++ find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
-    // Points to remember that any node which is a part of a cycle or leads to the cycle
-    // through an incoming edge towards the cycle, cannot be a safe node.
-    // Apart from these types of nodes, every node is a safe node.
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int> adj[graph.size()];
-        vector<int> indegree(graph.size(), 0);
+        int n = graph.size();
 
-        for(int i = 0; i < graph.size(); i++){
+        vector<int> adj[n];
+        vector<int> indegree(n,0);
+        // Reverse the direction
+        for(int i = 0; i<n; i++){
             for(auto it:graph[i]){
                 adj[it].push_back(i);
                 indegree[i]++;
             }
         }
 
-        vector<int> safeNodes;
         queue<int> q;
-        
-        for(int i = 0; i < indegree.size(); i++){
-            if(indegree[i] == 0){
+        for(int i = 0; i < n; i++){
+            if(indegree[i]==0){
                 q.push(i);
             }
         }
 
+        vector<int> safenodes;
         while(!q.empty()){
             int node = q.front();
             q.pop();
 
-            safeNodes.push_back(node);
+            safenodes.push_back(node);
             for(auto it:adj[node]){
-                if(indegree[it] > 0){
-                    indegree[it]--;
-                }
-                if(indegree[it] == 0){
+                indegree[it]--;
+                if(indegree[it]==0){
                     q.push(it);
                 }
             }
         }
-        sort(safeNodes.begin(), safeNodes.end());
-        return safeNodes;
+
+        sort(safenodes.begin(), safenodes.end());
+        return safenodes;
     }
 };
